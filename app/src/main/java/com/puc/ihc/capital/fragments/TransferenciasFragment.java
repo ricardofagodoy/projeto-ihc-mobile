5ce3ed05e91bd15ce18599e61e1945b8e074f4a2
afr-team-f <fepis_123@hotmail.com>
2015-05-23 04:12:46 -0300

    sexta a noite

--- app/src/main/java/com/puc/ihc/capital/fragments/TransferenciasFragment.java
+++ app/src/main/java/com/puc/ihc/capital/fragments/TransferenciasFragment.java
@@ -1,18 +1,40 @@
 package com.puc.ihc.capital.fragments;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.puc.ihc.capital.PrincipalActivity;
 import com.puc.ihc.capital.R;
+import com.puc.ihc.capital.adapters.ContaCorrenteAdapter;
+import com.puc.ihc.capital.adapters.TransferenciasAdapter;
 import com.puc.ihc.capital.interfaces.OnFragmentInteraction;
+import com.puc.ihc.capital.model.RegistroContaCorrente;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TransferenciasFragment extends Fragment {
 
     private OnFragmentInteraction mListener;
+    private int lastExpandedPosition = -1;
+    private TextView confirmacao;
 
     public TransferenciasFragment() {
     }
@@ -30,11 +52,70 @@ public class TransferenciasFragment extends Fragment {
         }*/
     }
 
+    public static void slide_down(Context ctx, View v){
+        Animation a = AnimationUtils.loadAnimation(ctx, R.anim.slide_down);
+        if(a != null){
+            a.reset();
+            if(v != null){
+                v.clearAnimation();
+                v.startAnimation(a);
+            }
+        }
+    }
+
+    public static void slide_up(Context ctx, View v){
+        Animation a = AnimationUtils.loadAnimation(ctx, R.anim.slide_up);
+        if(a != null){
+            a.reset();
+            if(v != null){
+                v.clearAnimation();
+                v.startAnimation(a);
+            }
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_transferencias, container, false);
+
+
+       final ExpandableListView lista = (ExpandableListView) view.findViewById(R.id.listTransferencias);
+
+        List<String> listDataHeader = new ArrayList<String>();
+        HashMap<String, List<String>> listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Antonio freitas");
+        listDataHeader.add("Gustavo Ribachaves");
+        listDataHeader.add("Chaponildo Caveira");
+
+        // Adding child data
+        List<String> valor = new ArrayList<String>();
+        valor.add("Valor: ");
+
+        listDataChild.put(listDataHeader.get(0), valor); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), valor);
+        listDataChild.put(listDataHeader.get(2), valor);
+
+        TransferenciasAdapter adapter = new TransferenciasAdapter(getActivity(), listDataHeader, listDataChild);
+        lista.setAdapter(adapter);
+
+        lista.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                if (lastExpandedPosition != -1
+                        && groupPosition != lastExpandedPosition) {
+                    lista.collapseGroup(lastExpandedPosition);
+                }
+                lastExpandedPosition = groupPosition;
+            }
+        });
+
+
+
         return view;
     }
 
@@ -43,6 +124,8 @@ public class TransferenciasFragment extends Fragment {
         super.onAttach(activity);
         //((PrincipalActivity) activity).onSectionAttached(getArguments().getString("Titulo"));
     }
+
+
 
     @Override
     public void onDetach() {
